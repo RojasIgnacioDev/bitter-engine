c089f51fb1c16c46eab70940cbdb8dc97f457613
Ignacio Rojas <ignaciorojasdev@hotmail.com>
2024-01-19 00:39:53 -0300

    added Shape2D class

--- Source/Shape2D.cs
+++ Source/Shape2D.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace BitterEngine.Source
 {
-    internal class Shape2D
+    public class Shape2D
     {
+        public Vector2 Position { get; set; } = null;
+        public Vector2 Scale { get; set; } = null;
+
+        public Shape2D(Vector2 position, Vector2 scale) {
+            Position = position;
+            Scale = scale;
+        }
     }
 }
