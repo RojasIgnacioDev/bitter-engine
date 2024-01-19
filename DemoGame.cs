f0c535ad430339adaf067b5f3f1f1dea2a65d886
Ignacio Rojas <ignaciorojasdev@hotmail.com>
2024-01-18 22:55:25 -0300

    created a game demo

---
+++ DemoGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BitterEngine.Source;
+
+namespace BitterEngine;
+class DemoGame : BitterEngine.Source.BitterEngine {
+    public DemoGame() : base(new Vector2(800, 600), "BitterEngine demo") { }
+
+    public override void OnLoad() {
+        Console.WriteLine("yay");
+    }
+
+    public override void OnUpdate() {
+
+        Console.WriteLine($"Frame {Frame}");
+        Frame++;
+    }
+
+    public override void OnDraw()
+    {
+
+    }
+}
